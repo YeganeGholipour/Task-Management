cabc75e5d8fe72619865f69a7d9c3503e5d448f3
YeganeGholipour <yeganegholiour@gmail.com>
2024-06-16 13:50:48 +0330

    fix: model and status choice

--- management/models.py
+++ management/models.py
@@ -3,8 +3,9 @@ from django.db import models
 
 class StatusChoice(models.TextChoices):
     PENDING = 'pending', 'Pending'
-    IN_PROGRESS = 'in_progress', 'In_Progress'
+    IN_PROGRESS = 'in_progress', 'In Progress'
     COMPLETED = 'completed', 'Completed'
+
 
 class Project(models.Model):
     name = models.CharField(max_length=255)
@@ -12,11 +13,12 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+
 class Task(models.Model):
-    Project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    status = models.CharField(choices=StatusChoice, default=StatusChoice.PENDING)
+    status = models.CharField(max_length=20, choices=StatusChoice.choices, default=StatusChoice.PENDING)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     due_date = models.DateTimeField()
