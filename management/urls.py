08db8b166da7de87b24926f6f93f5f442d883d81
YeganeGholipour <yeganegholiour@gmail.com>
2024-06-16 21:33:42 +0330

    add name for urls.py

--- management/urls.py
+++ management/urls.py
@@ -2,9 +2,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("projects/", views.ProjectView.as_view()),
-    path("projects/<int:pk>/", views.ProjectView.as_view()),
-    path("tasks/", views.TaskView.as_view()),
-    path("tasks/<int:pk>/", views.TaskView.as_view()),
-    path("tasks/<int:pk>/comments/", views.CommentsView.as_view()),
+    path("projects/", views.ProjectView.as_view(), name="projects"),
+    path("projects/<int:pk>/", views.ProjectView.as_view(), name="project"),
+    path("tasks/", views.TaskView.as_view(), name="tasks"),
+    path("tasks/<int:pk>/", views.TaskView.as_view(), name="task"),
+    path("tasks/<int:pk>/comments/", views.CommentsView.as_view(), name="comments"),
 ]
