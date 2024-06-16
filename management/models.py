becdf382502a2dab0c480e62517dcfa3c0e95553
YeganeGholipour <yeganegholiour@gmail.com>
2024-06-16 13:34:21 +0330

    add : models

---
+++ management/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class StatusChoice(models.TextChoices):
+    PENDING = 'pending', 'Pending'
+    IN_PROGRESS = 'in_progress', 'In_Progress'
+    COMPLETED = 'completed', 'Completed'
+
+class Project(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Task(models.Model):
+    Project = models.ForeignKey(Project, related_name='tasks', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(choices=StatusChoice, default=StatusChoice.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField()
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
